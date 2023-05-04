123f5c4f080147d0066344f35aa635a113e48d20
CyberpunkBoi <87060894+CyberpunkBoi@users.noreply.github.com>
2023-05-04 17:39:29 +0700

    Add files via upload

---
+++ v2/test_scripts/comments_tool.py
@@ -0,0 +1,53 @@
+from langchain.tools import tool
+
+import sqlalchemy
+from sqlalchemy import create_engine
+from sqlalchemy import Table, Column, Integer, String, MetaData, ForeignKey
+from sqlalchemy import inspect
+from sqlalchemy.sql import text
+from sqlalchemy.engine import URL
+
+
+@tool
+def get_comments(url: str) -> str:
+    """Retrieves all comments for tables and columns from database."""
+    engine = create_engine(url)
+    meta = MetaData()
+    MetaData.reflect(meta, bind = engine)
+
+    with engine.connect() as con:
+
+        statement_cols = text("""select
+    c.table_name,
+    c.column_name,
+    pgd.description
+from pg_catalog.pg_statio_all_tables as st
+inner join pg_catalog.pg_description pgd on (
+    pgd.objoid = st.relid
+)
+inner join information_schema.columns c on (
+    pgd.objsubid   = c.ordinal_position and
+    c.table_schema = st.schemaname and
+    c.table_name   = st.relname
+);""")
+    statement_tables = text("""SELECT obj_description(oid), relname
+FROM pg_class
+WHERE relkind = 'r'
+""")
+    response_cols = con.execute(statement_cols)
+    response_tables = con.execute(statement_tables)
+    results_cols = response_cols.mappings().all()
+    results_tables = response_tables.mappings().all()
+
+    return str([results_cols, results_tables])
+
+test_url = URL.create(
+    "postgresql",
+    username="gpt_bi_user",
+    password="uzH#Q%N9YvM3f!",  
+    host="62.109.28.9",
+    port="2401",
+    database="dwh_uas",
+)
+
+get_comments(test_url)
