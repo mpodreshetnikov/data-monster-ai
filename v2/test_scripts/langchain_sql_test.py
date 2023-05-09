4255cbbb21e8716231fdb30a951ea414e5950085
Maxim Podreshetnikov <mpodreshetnikov@gmail.com>
2023-05-09 14:43:53 +0700

    add usage of distinct formatting func

--- v2/test_scripts/langchain_sql_test.py
+++ v2/test_scripts/langchain_sql_test.py
@@ -9,10 +9,8 @@ from sqlalchemy.engine import URL
 import os
 
 from db_data_interaction.toolkit import DbDataInteractionToolkit
-from prompts.agent_prompts import SQL_PREFIX, SQL_SUFFIX
+from prompts.agent_prompts import SQL_SUFFIX, get_formatted_prefix_with_additional_info
 from monitoring.callback import DefaultCallbackHandler
-
-from utils.color import BOLD, END
 
 
 is_debug = True
@@ -41,37 +39,14 @@ while True:
     print("Задай вопрос: ")
     question = str(input())
     with get_openai_callback() as cb:
-        # Получаем подсказку для базы данных
-        db_hint = toolkit.get_db_hint(question)
-
-        # Получаем подсказки для запроса
-        query_hints = toolkit.get_query_hints(question, 2)
-
-        # Формируем строку с подсказками для вывода
-        query_hints_str = ''.join([f"{BOLD}Question:{END} {hint.question}\n{BOLD}Query:{END}\n{hint.query}" for hint in query_hints])
-
-        # Получаем уникальные таблицы из подсказок и получаем информацию о каждой таблице
-        unique_tables = list(set(table for hint in query_hints for table in hint.tables))
-        tables_info = ''.join(toolkit.get_table_info(table) for table in unique_tables)
-
-        # Формируем строку с информацией о таблицах и примерами похожих запросов для вывода
-        table_info_str = ('To answer the request, we have prepared a table/tables that you '
-                          'that you need to use especially for you: {BOLD}{unique_tables}{END}.\n'
-                          'Brief {BOLD}information about tables{END}: {tables_info}\n'
-                          .format(unique_tables=unique_tables, tables_info=tables_info, BOLD = BOLD, END = END ))
-        
-        query_hints_str = 'Examples of similar queries:\n{query_hints_str}'.format(query_hints_str = query_hints_str)
-
-        # Объединяем информацию о таблицах и примеры запросов в SQL_PREFIX
-        output_str = f"{table_info_str} {query_hints_str}"
-        SQL_PREFIX += output_str
-        print(SQL_PREFIX)   
+        agent_prefix = get_formatted_prefix_with_additional_info(toolkit, question, query_hints_limit=2)
+        print(agent_prefix)
         try:
             agent_executor = create_sql_agent(
                 llm=ChatOpenAI(verbose=is_debug),
                 toolkit=toolkit,
                 verbose=is_debug,
-                prefix=SQL_PREFIX,
+                prefix=agent_prefix,
                 suffix=SQL_SUFFIX,
             )
             response = agent_executor.run(question, callbacks=[DefaultCallbackHandler()])
