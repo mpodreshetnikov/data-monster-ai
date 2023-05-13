b5466f96da6deeaeae2c13285b50acde4cda62c1
GudimArt <gudimovartm@gmail.com>
2023-05-14 00:55:53 +0700

    Rename

--- v2/test_scripts/prompts/translator_prompts.py
+++ v2/test_scripts/prompts/translator_prompts.py
@@ -1,9 +1,9 @@
 from langchain.prompts import PromptTemplate
 
-template = """You are a translator from English to Russian.
+TRANSLATOR_TEMPLATE = """You are a translator from English to Russian.
 Translate this text:
 {text_to_translate}
 The final version of the translation:"""
 
 
-translator_prompt= PromptTemplate(input_variables=["text_to_translate"], template=template)
+TRANSLATOR_PROMPT= PromptTemplate(input_variables=["text_to_translate"], template=TRANSLATOR_TEMPLATE)
