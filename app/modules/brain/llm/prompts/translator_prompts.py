5b0a55f78657cb5b068137f5ed668ce6f2061dc4
GudimArt <gudimovartm@gmail.com>
2023-06-01 00:11:49 +0700

    change TRANSLATOR_PROMPT

--- app/modules/brain/llm/prompts/translator_prompts.py
+++ app/modules/brain/llm/prompts/translator_prompts.py
@@ -1,7 +1,35 @@
-from langchain.prompts import PromptTemplate
+from langchain import PromptTemplate, FewShotPromptTemplate
 
-__TRANSLATOR_TEMPLATE__ = """You are a translator from English to Russian.
-Translate this text:
-{text_to_translate}
-The final version of the translation:"""
-TRANSLATOR_PROMPT = PromptTemplate.from_template(__TRANSLATOR_TEMPLATE__)
+
+examples = [
+    {
+        "text": "Коровина Наталья Васильевна продала больше всех анальгина - 8 штук",
+        "tr_text": "Коровина Наталья Васильевна продала больше всех анальгина - 8 штук",
+    },
+    {"text": "The answer seems to be empty", "tr_text": "Ответ кажется пустым"},
+]
+
+example_formatter_template = """Text to be translated: {text}
+Translated text: {tr_text}
+"""
+
+TRANSLATOR_PREFIX = """You are a translator from English into Russian. If the text came in Russian, then you leave it in Russian"""
+
+TRANSLATOR_SUFFIX = """
+Text to be translated: {input}
+Translated text:
+"""
+
+example_prompt = PromptTemplate(
+    input_variables=["text", "tr_text"],
+    template=example_formatter_template,
+)
+
+TRANSLATOR_PROMPT = FewShotPromptTemplate(
+    examples=examples,
+    example_prompt=example_prompt,
+    prefix=TRANSLATOR_PREFIX,
+    suffix=TRANSLATOR_SUFFIX,
+    input_variables=["input"],
+    example_separator="\n",
+)
